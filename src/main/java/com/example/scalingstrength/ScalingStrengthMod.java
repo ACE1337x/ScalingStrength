b6a297713d98f88beca724d71385c91a266edd84
Vito Messina <vito.j.messina@gmail.com>
2024-11-20 21:47:41 -0500

    - Fixing mod to work outside of the IDE - Added logging - Code styling

--- src/main/java/com/example/scalingstrength/ScalingStrengthMod.java
+++ src/main/java/com/example/scalingstrength/ScalingStrengthMod.java
@@ -5,13 +5,14 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ScalingStrengthMod implements ModInitializer {
+public class ScalingStrengthMod implements ModInitializer
+{
     public static final String MOD_ID = "scalingstrength";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger( MOD_ID );
 
     @Override
-    public void onInitialize() {
-        // Your mod initialization code here
-        LOGGER.info("Scaling Strength Mod Initialized!");
+    public void onInitialize()
+    {
+        LOGGER.info("Initialized Scaling Strength Mod");
     }
 }
--- src/main/java/com/example/scalingstrength/mixin/StatusEffectsMixin.java
+++ src/main/java/com/example/scalingstrength/mixin/StatusEffectsMixin.java
@@ -1,6 +1,8 @@
 
 package com.example.scalingstrength.mixin;
 
+import com.example.scalingstrength.ScalingStrengthMod;
+import net.minecraft.entity.effect.DamageModifierStatusEffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.effect.StatusEffectCategory;
@@ -15,45 +17,66 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.reflect.Constructor;
 
-@Mixin(StatusEffects.class)
-public class StatusEffectsMixin {
-
+@Mixin( StatusEffects.class )
+public class StatusEffectsMixin
+{
     // Inject into the register method to modify the StatusEffect before it gets registered
-    @Inject(method = "register", at = @At("HEAD"), cancellable = true)
-    private static void onRegister(int rawId, String id, StatusEffect entry, CallbackInfoReturnable<StatusEffect> cir) {
-        try {
+    @Inject( method = "register", at = @At( "HEAD" ), cancellable = true )
+    private static void onRegister( int rawId, String id, StatusEffect entry, CallbackInfoReturnable<StatusEffect> cir )
+    {
+        try
+        {
             // Check if the effect being registered is the STRENGTH effect
-            if (id.equals("strength")) {
+            if( id.equals( "strength" ) )
+            {
+                ScalingStrengthMod.LOGGER.info( "Hooking 'StatusEffects.register' begin" );
 
                 // Access the DamageModifierStatusEffect class via reflection
-                Class<?> damageModifierClass = Class.forName("net.minecraft.entity.effect.DamageModifierStatusEffect");
+                Class< ? > damageModifierClass = DamageModifierStatusEffect.class;
+
+                ScalingStrengthMod.LOGGER.info( "Found 'DamageModifierStatusEffect' class" );
 
                 // Access the constructor of DamageModifierStatusEffect
-                Constructor<?> constructor = damageModifierClass.getDeclaredConstructor(StatusEffectCategory.class, int.class, double.class);
-                constructor.setAccessible(true);  // Make the constructor accessible
+                Constructor< ? > constructor = damageModifierClass.getDeclaredConstructor( StatusEffectCategory.class, int.class, double.class );
+
+                ScalingStrengthMod.LOGGER.info( "Setting 'DamageModifierStatusEffect' constructor accessible" );
+
+                constructor.setAccessible( true );  // Make the constructor accessible
+
+                ScalingStrengthMod.LOGGER.info( "Creating Strength Effect Override" );
 
                 // Instantiate DamageModifierStatusEffect using the constructor
-                StatusEffect modifiedStrengthEffect = (StatusEffect) constructor.newInstance(
-                        StatusEffectCategory.BENEFICIAL, 16762624, 0.2
+                StatusEffect strengthEffectOverride = (StatusEffect) constructor.newInstance(
+                        StatusEffectCategory.BENEFICIAL,
+                        16762624,
+                        0.2 // 20% increase per level
                 );
 
                 // Add a custom attribute modifier to scale the damage based on potion level
-                modifiedStrengthEffect.addAttributeModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE,
-                        "648D7064-6A60-4F59-8ABE-C2C23A6DD7A9",
-                        1.0,  // 20% increase per level
-                        EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
+                strengthEffectOverride.addAttributeModifier(
+                        EntityAttributes.GENERIC_ATTACK_DAMAGE,
+                        "648D7064-6A60-4F59-8ABE-C2C23A6DD7A9", // Same as vanilla Strength
+                        1.0,
+                        EntityAttributeModifier.Operation.MULTIPLY_TOTAL
+                );
+
+                ScalingStrengthMod.LOGGER.info( "Registering Strength Effect Override" );
 
                 // Register the modified STRENGTH effect instead of the original
-                Registry.register(Registries.STATUS_EFFECT, rawId, id, modifiedStrengthEffect);
+                Registry.register( Registries.STATUS_EFFECT, rawId, id, strengthEffectOverride );
 
                 // Return the modified effect instead of the original one
-                cir.setReturnValue(modifiedStrengthEffect);
+                cir.setReturnValue( strengthEffectOverride );
 
                 // Cancel the original registration to avoid registering the unmodified STRENGTH effect
                 cir.cancel();
+
+                ScalingStrengthMod.LOGGER.info( "Hooking 'StatusEffects.register' complete" );
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+        catch( Exception e )
+        {
+            ScalingStrengthMod.LOGGER.error( "Error while hooking 'StatusEffects.register'", e );
         }
     }
 }
